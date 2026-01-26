99ca33fac69d39225c21863b121e17f8738d70e5
Дмитрий <dimabresky@gmail.com>
2026-01-27 00:26:14 +0300

    chore(autoload): register task queue table
    Register TaskQueueTable in module autoload mapping.

--- include.php
+++ include.php
@@ -6,4 +6,5 @@ CModule::AddAutoloadClasses("travelsoft.tsoperator.amointegration", [
     "\\travelsoft\\amocrm\\EventsHandlers" => "lib/EventsHandlers.php",
     "\\travelsoft\\amocrm\\Option" => "lib/Option.php",
     "\\travelsoft\\amocrm\\tables\\LeadsTable" => "lib/tables/LeadsTable.php",
+    "\\travelsoft\\amocrm\\tables\\TaskQueueTable" => "lib/tables/TaskQueueTable.php",
 ]);
