225d9bf1abfc8412967770ec721530c63881c0eb
Дмитрий <dimabresky@gmail.com>
2026-01-27 12:14:37 +0300

    chore(autoload): add utils class mapping
    Register Utils in module autoload classes.

--- include.php
+++ include.php
@@ -5,6 +5,7 @@ require_once 'vendor/autoload.php';
 CModule::AddAutoloadClasses("travelsoft.tsoperator.amointegration", [
     "\\travelsoft\\amocrm\\EventsHandlers" => "lib/EventsHandlers.php",
     "\\travelsoft\\amocrm\\Option" => "lib/Option.php",
+    "\\travelsoft\\amocrm\\Utils" => "lib/Utils.php",
     "\\travelsoft\\amocrm\\tables\\LeadsTable" => "lib/tables/LeadsTable.php",
     "\\travelsoft\\amocrm\\tables\\TaskQueueTable" => "lib/tables/TaskQueueTable.php",
 ]);
